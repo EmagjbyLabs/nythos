76d63ec3a83a36adfed3b77959cdd31ed1b72d88
Emagjby <emagjby@gmail.com>
2026-06-07 18:22:48 +0300

    feat(ports): add external identity repository port

--- tests/support/mod.rs
+++ tests/support/mod.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
+pub mod external_identity_repo;
 pub mod fixtures;
 pub mod role_repo;
 pub mod security;
@@ -8,6 +9,7 @@ pub mod session_store;
 pub mod tenant_policy;
 pub mod user_repo;
 
+pub use external_identity_repo::InMemoryExternalIdentityRepository;
 pub use role_repo::InMemoryRoleRepository;
 pub use security::{FakePasswordHasher, FakeRevocationChecker, FakeTokenSigner};
 pub use session_store::InMemorySessionStore;
